8abff539afcada848fade1ddf003ab7fb49a384e
Robert Kwasny <robertkwasny@Roberts-MacBook-Pro.local>
2021-11-29 09:57:12 +0100

    set-up hw3

---
+++ HW3/argparse.py
@@ -0,0 +1,20 @@
+ # Robert Kwasny
+ # 29/11/2021
+
+import sys
+import argparse
+
+
+
+
+
+# Create program which take as argument one existing file, one integer number and one result file as in below help. (All with default parameters)
+#
+# Usage: exercise_1.py [-h] [-i INPUT] [-n NUM] [-o OUTPUT]
+#
+# optional arguments:
+# -h, --help            show this help message and exit
+# -i INPUT, --input INPUT
+# -n NUM, --num NUM
+# -o OUTPUT, --output OUTPUT
+# program should print values of input, num and output on standard output
